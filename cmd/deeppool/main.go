fd4eb3b85e5d08834d322608c0154ea5352c8e09
James Lawrence <jljatone@gmail.com>
2024-11-05 07:25:19 -0500

    working io reader info

--- cmd/deeppool/main.go
+++ cmd/deeppool/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/james-lawrence/torrent"
+	"github.com/james-lawrence/torrent/metainfo"
 )
 
 func main() {
@@ -12,10 +13,19 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	md, err := torrent.NewFromReader(src)
+	minfo, err := metainfo.NewFromReader(src, metainfo.OptionDisplayName("hello.world.txt"))
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	log.Println("magnet uri:", torrent.NewMagnet(md).String())
+	md1, err := torrent.NewFromInfo(*minfo)
+	// md1, err := torrent.NewFromReader(src, torrent.OptionDisplayName("hello.world.txt"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("magnet uri:", torrent.NewMagnet(md1).String())
+	md2, err := torrent.NewFromFile("hello.world.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("magnet uri:", torrent.NewMagnet(md2).String())
 }
