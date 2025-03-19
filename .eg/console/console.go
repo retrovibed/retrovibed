27b342aa079e49ad6c6cfe1835298617e517038a
James Lawrence <jljatone@gmail.com>
2025-03-19 10:53:06 -0400

    update packaging

--- .eg/console/console.go
+++ .eg/console/console.go
@@ -66,7 +66,7 @@ func Install(ctx context.Context, op eg.Op) error {
 
 func flatpak(final egflatpak.Module) *egflatpak.Builder {
 	return egflatpak.New(
-		"space.retrovibe.Console", "retrovibe",
+		"space.retrovibe", "console",
 		egflatpak.Option().SDK("org.gnome.Sdk", "47").Runtime("org.gnome.Platform", "47").
 			Modules(
 				libassmodule(),
