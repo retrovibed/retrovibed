dd0522e93366d1692cf08bf6353a15386d4a840e
James Lawrence <jljatone@gmail.com>
2025-03-20 13:31:05 -0400

    increase timeout for filesystem shutdown to one minute

--- shallows/library/import.filesystem.go
+++ shallows/library/import.filesystem.go
@@ -147,7 +147,7 @@ func ImportFilesystem(ctx context.Context, op ImportOp, paths ...string) iter.Se
 		var err error
 		go func() {
 			defer func() {
-				ictx, done := context.WithTimeout(context.Background(), 10*time.Second)
+				ictx, done := context.WithTimeout(context.Background(), time.Minute)
 				defer done()
 				err = errorsx.Compact(err, asynccompute.Shutdown(ictx, arena))
 				log.Println("completed", err)
