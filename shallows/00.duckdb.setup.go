32b93a5ba95ff9dd5be890b68161a48598bba22f
James Lawrence <jljatone@gmail.com>
2025-03-20 12:11:35 -0400

    fix migrations reference

--- shallows/00.duckdb.setup.go
+++ shallows/00.duckdb.setup.go
@@ -1,4 +1,4 @@
 package deeppool
 
-//go:generate genieql duckdb --database=dpool.db ./cmd/retrovibed/.migrations
+//go:generate genieql duckdb --database=dpool.db ./cmd/cmdmeta/.migrations
 //go:generate genieql bootstrap --queryer=sqlx.Queryer --driver=github.com/marcboeker/go-duckdb duckdb://localhost/dpool.db
