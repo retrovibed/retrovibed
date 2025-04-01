9acbdf6c1f95018f0bc24a1211527a45579e9745
James Lawrence <jljatone@gmail.com>
2025-04-01 12:17:19 -0400

    verify data before resuming

--- shallows/tracking/metadata.go
+++ shallows/tracking/metadata.go
@@ -94,6 +94,19 @@ func MetadataSearchBuilder() squirrel.SelectBuilder {
 	return squirrelx.PSQL.Select(sqlx.Columns(MetadataScannerStaticColumns)...).From("torrents_metadata")
 }
 
+func Verify(ctx context.Context, t torrent.Torrent) error {
+	log.Println("verify initiated", t.Name())
+	defer log.Println("verify completed", t.Name())
+
+	select {
+	case <-t.GotInfo():
+		t.VerifyData()
+		return nil
+	case <-ctx.Done():
+		return errorsx.Compact(context.Cause(ctx), ctx.Err())
+	}
+}
+
 func Download(ctx context.Context, q sqlx.Queryer, vfs fsx.Virtual, md *Metadata, t torrent.Torrent) (err error) {
 	var (
 		downloaded int64
