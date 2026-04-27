d882375e2a6ea645755499b2a85956f73222664e
James Lawrence <jljatone@gmail.com>
2026-04-27 10:05:22 -0400

    rename source archive

--- .eg/tarballs/tarballs.go
+++ .eg/tarballs/tarballs.go
@@ -30,7 +30,7 @@ func Retrovibed(b *Build) string {
 }
 
 func RetrovibedSource() string {
-	return "retrovibed.tar.gz"
+	return "retrovibed.source"
 }
 
 func Flatpak(b *Build) string {
@@ -40,7 +40,7 @@ func Flatpak(b *Build) string {
 func Tarchive(ctx context.Context, op eg.Op) error {
 	return eg.Sequential(
 		shell.Op(
-			shell.Newf("git archive --format=tar.gz -o %s %s", egtarball.Path(RetrovibedSource()), eggit.EnvCommit().StringReplace("%git.hash%")),
+			shell.Newf("git archive --format=tar.xz -o %s %s", egtarball.Path(RetrovibedSource()), eggit.EnvCommit().StringReplace("%git.hash%")),
 		),
 	)(ctx, op)
 }
