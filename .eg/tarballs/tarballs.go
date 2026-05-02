82f58749e52db70d8e6436d36eda50c1727ca8d7
James Lawrence <jljatone@gmail.com>
2026-05-02 04:05:48 -0400

    appimage setup

--- .eg/tarballs/tarballs.go
+++ .eg/tarballs/tarballs.go
@@ -37,6 +37,30 @@ func Flatpak(b *Build) string {
 	return b.StringReplace("space.retrovibe.Console.yml")
 }
 
+func AppImage(b *Build) string {
+	return b.StringReplace("retrovibed.%goenv.os%.%goenv.arch%.AppImage")
+}
+
+// Version returns the date-based build version string from git commit metadata.
+func Version() string {
+	return eggit.EnvCommit().StringReplace("%git.commit.year%.%git.commit.month%.%git.commit.day%")
+}
+
+// ArchGoToMachine maps GOARCH/dpkg architecture names (amd64, arm64, 386, arm)
+// to POSIX uname -m machine hardware names (x86_64, aarch64, i686, armhf).
+func ArchGoToMachine(arch string) string {
+	switch arch {
+	case "arm64":
+		return "aarch64"
+	case "386":
+		return "i686"
+	case "arm":
+		return "armhf"
+	default: // amd64
+		return "x86_64"
+	}
+}
+
 func Tarchive(ctx context.Context, op eg.Op) error {
 	return eg.Sequential(
 		shell.Op(
