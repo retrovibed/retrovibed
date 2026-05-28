a39e0c8efed692f1816b9e09d98941d14e595b20
James Lawrence <jljatone@gmail.com>
2026-05-28 09:30:41 -0400

    build improvements

--- .eg/dev/console/linux/main.go
+++ .eg/dev/console/linux/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
 	"github.com/egdaemon/eg/runtime/wasi/eggit"
 	"github.com/egdaemon/eg/runtime/wasi/shell"
-	"github.com/egdaemon/eg/runtime/x/wasi/egbug"
 )
 
 func main() {
@@ -30,18 +29,13 @@ func main() {
 			ctx,
 			deb,
 			eg.Sequential(
-				egbug.Log("DERP DERP DERP 0"),
 				eg.Parallel(
 					duckdb.MaybeBuild(filepath.Join("dev.native.libs", "libduckdb_bundle.a"), duckdb.Compile(shell.Runtime()), duckdb.Clone),
 					neurals.MaybeBuild(filepath.Join("dev.native.libs", "libpredicttext.a"), neurals.Compile, neurals.Clone),
 				),
-				egbug.Log("DERP DERP DERP 1"),
 				console.GenerateDevStaticBinding(shell.Runtime(), egenv.WorkingDirectory("console", "build", "nativelib"), egenv.CacheDirectory("dev.native.libs")),
-				egbug.Log("DERP DERP DERP 2"),
 				console.Generate,
-				egbug.Log("DERP DERP DERP 3"),
 				console.BuildLinux,
-				egbug.Log("DERP DERP DERP 4"),
 			),
 		),
 		console.RunDev("flutter run -d linux"),
