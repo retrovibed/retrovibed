ee334f01314e556058af405f82122f9b026943b0
James Lawrence <jljatone@gmail.com>
2026-05-29 16:14:04 -0400

    update ios

--- .eg/release/ios/main.go
+++ .eg/release/ios/main.go
@@ -38,8 +38,10 @@ func main() {
 	err := eg.Perform(
 		ctx,
 		eg.Sequential(
-			duckdb.MaybeBuild(".eg.cache/duckdb/.arm64/libduckdb_static.a", duckdb.CompileIOSRuntime("ios_arm64", "arm64"), duckdb.CompileIOS, duckdb.CloneStaticBuild),
-			neurals.CompileIOS(egenv.WorkingDirectory("console/ios")),
+			eg.Parallel(
+				duckdb.MaybeBuild(".eg.cache/duckdb/.arm64/libduckdb.a", duckdb.CompileIOSRuntime("ios_arm64", "arm64"), duckdb.CompileIOS, duckdb.CloneStaticBuild),
+				neurals.CompileIOS(egenv.WorkingDirectory("console/ios")),
+			),
 			shell.Op(shell.Newf("cp .eg.cache/duckdb/.arm64/*.a console/ios/")),
 			console.GenerateFlutter,
 			egbug.DebugFailure(
