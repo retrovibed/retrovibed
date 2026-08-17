7a7693c7a3aef9cc4de604f61ba3e1c596567937
James Lawrence <jljatone@gmail.com>
2026-08-17 15:03:16 -0400

    release adjustments while github is down

--- .eg/release/automatic/main.go
+++ .eg/release/automatic/main.go
@@ -30,10 +30,10 @@ func main() {
 			debian.Release,
 		),
 		shell.Op(
-			shell.New("gh workflow run release.ios.yml --ref main").Attempts(3),
-			shell.New("gh workflow run release.macosx.yml --ref main").Attempts(3),
+			// shell.New("gh workflow run release.ios.yml --ref main").Attempts(3),
+			// shell.New("gh workflow run release.macosx.yml --ref main").Attempts(3),
 			shell.Env().New("eg login --seed=\"${RETROVIBED_EG_LOGIN_SEED}\""),
-			shell.Env().New("eg compute upload release/linux").Attempts(3),
+			shell.Env().New("eg compute upload --arch=amd64 release/linux").Attempts(3),
 			shell.Env().New("eg compute upload --arch=arm64 --cores=3 --memory=2g release/linux").Attempts(3),
 			shell.Env().New("eg compute upload release/retrokiosk").Attempts(3),
 			shell.Env().New("eg compute upload -e EG_SSH_KEY_SEED=${EG_SSH_KEY_SEED} release/archlinux").Attempts(3),
