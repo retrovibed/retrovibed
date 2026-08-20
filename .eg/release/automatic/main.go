dbc1151e336a713646965607b41306a8aa36ed26
James Lawrence <jljatone@gmail.com>
2026-08-20 17:28:40 -0400

    debugging arm64

--- .eg/release/automatic/main.go
+++ .eg/release/automatic/main.go
@@ -32,7 +32,7 @@ func main() {
 				shell.Env().New("gh workflow run release.macosx.yml --ref main").Attempts(3),
 				shell.Env().New("eg login --seed=\"${RETROVIBED_EG_LOGIN_SEED}\""),
 				shell.Env().New("eg compute upload --arch=amd64 release/linux").Attempts(3),
-				shell.Env().New("eg compute upload --arch=arm64 --cores=3 --memory=2g release/linux").Attempts(3),
+				shell.Env().New("eg compute upload --ttl=3h --arch=arm64 --cores=3 --memory=2g release/linux").Attempts(3),
 				shell.Env().New("eg compute upload -e EG_SSH_KEY_SEED=${EG_SSH_KEY_SEED} release/archlinux").Attempts(3),
 				shell.Env().New("eg compute upload -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/retrokiosk").Attempts(3).Debug(),
 				// shell.Env().New("eg compute upload release/android"), // TODO: android requires gcp credentials.
