c34a98d0ab482fd56f0cfd03485c3b3a37d24012
James Lawrence <jljatone@gmail.com>
2026-08-22 14:42:18 -0400

    remove debian build container in favor up uploading release/debian
    automate the debian release.

--- .eg/release/automatic/main.go
+++ .eg/release/automatic/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"eg/compute/egx"
-	"eg/compute/maintainer"
 	"log"
 	"time"
 
@@ -18,13 +17,9 @@ func main() {
 	ctx, done := context.WithTimeout(context.Background(), egenv.TTL())
 	defer done()
 
-	deb := eg.Container(maintainer.Container)
 	err := eg.Perform(
 		ctx,
 		eggit.AutoClone,
-		eg.Parallel(
-			eg.Build(deb.BuildFromFile(".eg/Containerfile")),
-		),
 		eg.Parallel(
 			shell.Op(
 				// environment propagation messed up -e {NAME} should be sufficient to copy values in.
@@ -34,28 +29,25 @@ func main() {
 				shell.Env().New("eg login --seed=\"${RETROVIBED_EG_LOGIN_SEED}\""),
 				shell.Env().New("eg compute upload --ttl=1h --arch=amd64 release/linux").Attempts(3),
 				shell.Env().New("eg compute upload --ttl=3h --arch=arm64 --cores=3 --memory=2g release/linux").Attempts(3),
-				shell.Env().New("eg compute upload -e EG_SSH_KEY_SEED=${EG_SSH_KEY_SEED} release/archlinux").Attempts(3),
-				shell.Env().New("eg compute upload -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/retrokiosk").Attempts(3),
+				shell.Env().New("eg compute upload --arch=amd64 -e EG_SSH_KEY_SEED=${EG_SSH_KEY_SEED} release/archlinux").Attempts(3),
+				shell.Env().New("eg compute upload --arch=amd64 -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/retrokiosk").Attempts(3),
+				shell.Env().New("eg compute upload --arch=amd64 -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/debian").Attempts(3),
 				// shell.Env().New("eg compute upload release/android"), // TODO: android requires gcp credentials.
 			),
 		),
-		eg.Module(
-			ctx,
-			deb,
-			eg.Sequential(
-				egx.RetryUntilSuccess(
-					30*time.Second,
-					eggithub.Promote(
-						"PKGBUILD",
-						"retrovibed.darwin.arm64.dmg",
-						"retrovibed.linux.amd64.AppImage",
-						"retrovibed.linux.amd64.AppImage.zsync",
-						"retrovibed.linux.amd64.tar.xz",
-						"retrovibed.linux.arm64.AppImage",
-						"retrovibed.linux.arm64.AppImage.zsync",
-						"retrovibed.linux.arm64.tar.xz",
-						"retrovibed.source.tar.gz",
-					),
+		eg.Sequential(
+			egx.RetryUntilSuccess(
+				30*time.Second,
+				eggithub.Promote(
+					"PKGBUILD",
+					"retrovibed.darwin.arm64.dmg",
+					"retrovibed.linux.amd64.AppImage",
+					"retrovibed.linux.amd64.AppImage.zsync",
+					"retrovibed.linux.amd64.tar.xz",
+					"retrovibed.linux.arm64.AppImage",
+					"retrovibed.linux.arm64.AppImage.zsync",
+					"retrovibed.linux.arm64.tar.xz",
+					"retrovibed.source.tar.gz",
 				),
 			),
 		),
