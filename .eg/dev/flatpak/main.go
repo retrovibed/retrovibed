2b3388b38583c1bb37c0c6d02370a626ecc99d68
James Lawrence <jljatone@gmail.com>
2026-08-23 19:22:45 -0400

    fix flatpak

--- .eg/dev/flatpak/main.go
+++ .eg/dev/flatpak/main.go
@@ -2,18 +2,11 @@ package main
 
 import (
 	"context"
-	"eg/compute/console"
-	"eg/compute/maintainer"
-	"eg/compute/release"
-	"eg/compute/shallows"
-	"eg/compute/tarballs"
 	"log"
 
 	"github.com/egdaemon/eg/runtime/wasi/eg"
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
-	"github.com/egdaemon/eg/runtime/wasi/eggit"
-	"github.com/egdaemon/eg/runtime/wasi/shell"
-	"github.com/egdaemon/eg/runtime/x/wasi/egtarball"
+	"github.com/egdaemon/eg/runtime/x/wasi/egbug"
 )
 
 // used to test flatpak build.
@@ -21,31 +14,33 @@ func main() {
 	ctx, done := context.WithTimeout(context.Background(), egenv.TTL())
 	defer done()
 
-	deb := eg.Container(maintainer.Container)
+	// deb := eg.Container(maintainer.Container)
 	err := eg.Perform(
 		ctx,
-		eggit.AutoClone,
-		eg.Build(deb.BuildFromFile(".eg/Containerfile")),
-		eg.Module(
-			ctx, deb,
-			eg.Sequential(
-				eg.Parallel(
-					eg.Sequential(console.GenerateBinding, console.BuildLinux),
-					shallows.Compile(),
-				),
-				eg.Parallel(
-					console.Install,
-					shallows.Install,
-					shell.Op(
-						shell.Newf("cp --verbose -R .dist/linux/* %s", egtarball.Path(tarballs.Retrovibed())),
-					),
-				),
-				release.Tarball,
-				eg.Parallel(
-					console.FlatpakBuild,
-				),
-			),
-		),
+		egbug.Log("disabled"),
+		egbug.Fail,
+		// eggit.AutoClone,
+		// eg.Build(deb.BuildFromFile(".eg/Containerfile")),
+		// eg.Module(
+		// 	ctx, deb,
+		// 	eg.Sequential(
+		// 		eg.Parallel(
+		// 			eg.Sequential(console.GenerateBinding, console.BuildLinux),
+		// 			shallows.Compile(),
+		// 		),
+		// 		eg.Parallel(
+		// 			console.Install,
+		// 			shallows.Install,
+		// 			shell.Op(
+		// 				shell.Newf("cp --verbose -R .dist/linux/* %s", egtarball.Path(tarballs.Retrovibed())),
+		// 			),
+		// 		),
+		// 		release.Tarball,
+		// 		eg.Parallel(
+		// 			console.FlatpakBuild,
+		// 		),
+		// 	),
+		// ),
 	)
 
 	if err != nil {
