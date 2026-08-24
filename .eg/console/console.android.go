0a41450419af3246a92d576bbefb9a864454d1b9
James Lawrence <jljatone@gmail.com>
2026-08-24 08:50:36 -0400

    filter out arm arch from flutter

--- .eg/console/console.android.go
+++ .eg/console/console.android.go
@@ -36,7 +36,7 @@ func BuildAndroidAPK(runtime shell.Command) eg.OpFn {
 		return shell.Run(
 			ctx,
 			runtime.New(
-				fmt.Sprintf("flutter build apk --build-name=%s --build-number=%s --release lib/main.dart", tarballs.Version(), commit.StringReplace("%git.commit.unix%")),
+				fmt.Sprintf("flutter build apk --target-platform android-arm64,android-x64 --build-name=%s --build-number=%s --release lib/main.dart", tarballs.Version(), commit.StringReplace("%git.commit.unix%")),
 			).Timeout(20*time.Minute),
 			runtime.New("mv app-release.apk retrovibed.apk").Timeout(20*time.Minute).Directory(egenv.WorkingDirectory("console/build/app/outputs/apk/release")),
 		)
@@ -51,7 +51,7 @@ func BuildAndroidBundle(runtime shell.Command) eg.OpFn {
 		return shell.Run(
 			ctx,
 			runtime.New(
-				fmt.Sprintf("flutter build appbundle --build-name=%s --build-number=%s --release lib/main.dart", tarballs.Version(), commit.StringReplace("%git.commit.unix%")),
+				fmt.Sprintf("flutter build appbundle --target-platform android-arm64,android-x64 --build-name=%s --build-number=%s --release lib/main.dart", tarballs.Version(), commit.StringReplace("%git.commit.unix%")),
 			).Timeout(20*time.Minute),
 			runtime.New("mv app-release.aab retrovibed.aab").Timeout(20*time.Minute).Directory(egenv.WorkingDirectory("console/build/app/outputs/bundle/release")),
 		)
