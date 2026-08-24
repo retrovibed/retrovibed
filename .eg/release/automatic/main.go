cdc8556819bc6cf921e8e381c3666196603c45d4
James Lawrence <jljatone@gmail.com>
2026-08-24 18:26:34 -0400

    reduce other releases resource requirements

--- .eg/release/automatic/main.go
+++ .eg/release/automatic/main.go
@@ -30,9 +30,9 @@ func main() {
 			shell.Env().New("gh workflow run release.macosx.yml --ref main").Attempts(3),
 			shell.Env().New("eg compute upload --ttl=1h --arch=amd64 --cores=8 --memory=12g release/linux").Attempts(3),
 			shell.Env().New("eg compute upload --ttl=3h --arch=arm64 --cores=3 --memory=3g release/linux").Attempts(3),
-			shell.Env().New("eg compute upload --arch=amd64 -e EG_SSH_KEY_SEED=${EG_SSH_KEY_SEED} release/archlinux").Attempts(3),
-			shell.Env().New("eg compute upload --arch=amd64 -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/retrokiosk").Attempts(3),
-			shell.Env().New("eg compute upload --arch=amd64 -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/debian").Attempts(3),
+			shell.Env().New("eg compute upload --arch=amd64 --cores=8 --memory=6g -e EG_SSH_KEY_SEED=${EG_SSH_KEY_SEED} release/archlinux").Attempts(3),
+			shell.Env().New("eg compute upload --arch=amd64 --cores=8 --memory=6g -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/retrokiosk").Attempts(3),
+			shell.Env().New("eg compute upload --arch=amd64 --cores=8 --memory=6g -e EG_GPG_KEYRING_NAME=\"${EG_GPG_KEYRING_NAME}\" -e EG_GPG_KEYRING_EMAIL=\"${EG_GPG_KEYRING_EMAIL}\" -e EG_GPG_KEYRING_SEED=\"${EG_GPG_KEYRING_SEED}\" release/debian").Attempts(3),
 			// shell.Env().New("eg compute upload release/android"), // TODO: android requires gcp credentials.
 		),
 		eg.Sequential(
