075ea318ca75253a2487ce491b16af473728d148
James Lawrence <jljatone@gmail.com>
2026-08-25 07:56:57 -0400

    debug debian failures

--- .eg/debian/daemon.go
+++ .eg/debian/daemon.go
@@ -12,6 +12,7 @@ import (
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
 	"github.com/egdaemon/eg/runtime/wasi/eggit"
 	"github.com/egdaemon/eg/runtime/wasi/shell"
+	"github.com/egdaemon/eg/runtime/x/wasi/egbug"
 	"github.com/egdaemon/eg/runtime/x/wasi/egdebuild"
 	"github.com/egdaemon/eg/runtime/x/wasi/eggolang"
 	"github.com/egdaemon/eg/runtime/x/wasi/eggpg"
@@ -67,7 +68,10 @@ func Build(ctx context.Context, o eg.Op) error {
 		// override eg compute local's setting of the home directory.
 		// ideally we should default to this except when performing local
 		// compute workloads.
-		eggpg.Seed(eggpg.Options().Home("/home/egd/.gnupg")...),
+		egbug.DebugFailure(
+			eggpg.Seed(eggpg.Options().Home("/home/egd/.gnupg")...),
+			eggpg.Debug(eggpg.Options().Home("/home/egd/.gnupg")...),
+		),
 		egdebuild.Build(gcfg, egdebuild.Option.Distro(egdebuild.UbuntuLatestCodename), egdebuild.Option.NoLint()), // resolute
 		egdebuild.Build(
 			gcfg,
