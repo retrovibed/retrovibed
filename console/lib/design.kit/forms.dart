bd181ab92060257ffc5cc969053a4331700bc077
James Lawrence <jljatone@gmail.com>
2026-08-27 11:56:14 -0400

    added a standard typedefs for forms.
    we have the form return a future to support api calls.

--- console/lib/design.kit/forms.dart
+++ console/lib/design.kit/forms.dart
@@ -3,3 +3,7 @@ export './forms/container.dart';
 export './forms/input.checkbox.dart';
 export './forms/item.management.dart';
 export './forms/presets.dart';
+
+typedef FnOnChange<T> = Future<T> Function(T v);
+
+Future<T> FnOnChangeNoop<T>(T v) => Future.value(v);
