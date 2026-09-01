732bcc2e19b8ae593306596b32e1b5f7a815143a
James Lawrence <jljatone@gmail.com>
2026-09-01 10:29:40 -0400

    pull in macos dev fixes.

--- .dist/duckdb_config.cmake
+++ .dist/duckdb_config.cmake
@@ -1,14 +1,3 @@
-duckdb_extension_load(inet)
-duckdb_extension_load(autocomplete)
-duckdb_extension_load(json)
-duckdb_extension_load(parquet)
-duckdb_extension_load(icu)
-duckdb_extension_load(vss
-    GIT_URL https://github.com/duckdb/duckdb-vss
-    GIT_TAG b833341c8737fd3f3558c7720cc575ae8fc82598
-    APPLY_PATCHES
-)
-
 set(BUILD_UNITTESTS 0 CACHE BOOL "Disable unit tests" FORCE)
 set(BUILD_SHELL 0 CACHE BOOL "Disable CLI shell" FORCE)
 set(ENABLE_EXTENSION_AUTOLOADING 1 CACHE BOOL "Enable extension autoloading" FORCE)
