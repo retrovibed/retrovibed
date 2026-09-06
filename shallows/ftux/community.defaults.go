38c72261c8789e753dad02350b3cf3b9d44c3f33
James Lawrence <jljatone@gmail.com>
2026-09-06 05:57:45 -0400

    add publishing plugins to ftux defaults

--- shallows/ftux/community.defaults.go
+++ shallows/ftux/community.defaults.go
@@ -14,14 +14,19 @@ func PrepareDefaultCommunities() (_ []*communityapi.Community, err error) {
 	encoded, err := fsx.AutoCached(userx.DefaultConfigDir(userx.DefaultRelRoot(), "default.communities.json"), func() ([]byte, error) {
 		return jsonx.Marshal([]*communityapi.Community{
 			{
-				Id:          "4b43c380-89e5-44f0-a5a1-c4e0b52a4bef",
-				Description: "Retrovibed - media metadata. posters, ratings, descriptions. (~3 GiB)",
-				Url:         community.CommunityURLFromDomain("media"),
-			},
-			{
 				Id:          "e54c2d7b-70f9-41fb-92f7-eeb0968f4be4",
 				Description: "Retrovibed - neurals enabling various small AI driven functionality for retrovibed",
 				Url:         community.CommunityURLFromDomain("neurals"),
+			},
+			{
+				Id:          "8a2e8383-0e50-40a1-8af6-69d4f8c67cb0",
+				Description: "Retrovibed - content publishing support",
+				Url:         community.CommunityURLFromDomain("retropublish"),
+			},
+			{
+				Id:          "4b43c380-89e5-44f0-a5a1-c4e0b52a4bef",
+				Description: "Retrovibed - media metadata. posters, ratings, descriptions. recommended for desktop/mobile. (~3 GiB)",
+				Url:         community.CommunityURLFromDomain("media"),
 			},
 		})
 
